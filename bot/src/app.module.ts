1b8d17747b0d8f902f6c190b9d805ddfe4a25cc4
capiban2 <randybenq@gmail.com>
2024-04-11 11:52:45 +0300

    dev: Updated package.json and imports

--- bot/src/app.module.ts
+++ bot/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StartModule } from './start/start.module';
+import { UtilityModule } from './utility/utility.module';
+import { MessageModule } from './message/message.module';
 
 @Module({
-  imports: [StartModule],
+  imports: [StartModule, UtilityModule, MessageModule],
   controllers: [AppController],
   providers: [AppService],
 })
