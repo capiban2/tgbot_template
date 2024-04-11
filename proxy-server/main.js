acea0ed208a560df60e4febf73e2194b696a4686
capiban2 <randybenq@gmail.com>
2024-04-11 08:52:23 +0300

    For webhook correction

--- proxy-server/main.js
+++ proxy-server/main.js
@@ -5,21 +5,22 @@ const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fet
 app.use(bodyParser.json());
 
 
-app.post('/', async (req, res) => {
+app.all('/', async (req, res) => {
   req.body.new_field = "new_data_to_field";
   const new_method = "POST";
 
-  const response = await fetch(
-    'http://bot:3000/start', {
-    method: new_method,
-    body: JSON.stringify(req.body),
-    headers: { 'Content-Type': 'application/json' }
-  });
-
-  console.log("RESP");
-  // console.log(resp);
-  res.set('Content-Type', 'application/json');
-  res.send(await response.json());
+  // const response = await fetch(
+  //   'http://bot:3000/start', {
+  //   method: new_method,
+  //   body: JSON.stringify(req.body),
+  //   headers: { 'Content-Type': 'application/json' }
+  // });
+  //
+  // console.log("RESP");
+  // // console.log(resp);
+  // res.set('Content-Type', 'application/json');
+  // res.send(await response.json());
+  res.sendStatus(201);
 
 
 });
