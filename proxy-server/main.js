b2723fd01570148c1c0105c6fe53bb78f1f0c9d9
capiban2 <randybenq@gmail.com>
2024-04-11 11:53:49 +0300

    Adjustings in proxy-serv main.js

--- proxy-server/main.js
+++ proxy-server/main.js
@@ -4,23 +4,47 @@ const bodyParser = require('body-parser');
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 app.use(bodyParser.json());
 
+function _handleMessageCase(req) {
+  if (req.message.text == undefined)
+    return "/message/silent"
+
+  if (req.message.text == "/start")
+    return "/message/start"
+  return "/message/smthdifferent"
+}
+function _handleInlineQuery(req) {
+  return "/message/start"
+}
+
+function _handleCallback(req) {
+  return `/callback/${req.callback_query.data}`
+}
+
+function chooseProperEndpoint(req) {
+  if (req.message !== undefined)
+    return _handleMessageCase(req);
+  if (req.chosen_inline_result !== undefined)
+    return _handleInlineQuery(req);
+  return _handleCallback(req);
+
+}
 
 app.all('/', async (req, res) => {
-  req.body.new_field = "new_data_to_field";
-  const new_method = "POST";
 
-  // const response = await fetch(
-  //   'http://bot:3000/start', {
-  //   method: new_method,
-  //   body: JSON.stringify(req.body),
-  //   headers: { 'Content-Type': 'application/json' }
-  // });
-  //
-  // console.log("RESP");
-  // // console.log(resp);
-  // res.set('Content-Type', 'application/json');
-  // res.send(await response.json());
-  res.sendStatus(201);
+  const new_method = "GET";
+  const endpoint = chooseProperEndpoint(req);
+
+  const response = await fetch(
+    `http://bot:3000/${endpoint}`, {
+    method: new_method,
+    body: JSON.stringify(req.body),
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+
+  res.set('Content-Type', 'application/json');
+  res.send(await response.json());
+
 
 
 });
